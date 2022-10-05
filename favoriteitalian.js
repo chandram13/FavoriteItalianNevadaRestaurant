8cf5c3aaf4d0f83d3dcfdd76b4445e575c11326d
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-05 13:59:16 -0700

    Add files via upload

--- favoriteitalian.js
+++ favoriteitalian.js
@@ -1,6 +1,7 @@
 // Marvish Chandra
 
-class appetizers{
+class favoriteItalian{
+function appetizers(){
     fritto_misto_italiano = 13.50;
     calamariFritti = 13.95;
     olive_Allascolana = 13.95;
@@ -9,7 +10,7 @@ class appetizers{
     Cocktail_Di_Gamberi = 16.60;
     Salumeria_Da_Matteo = "Create your own, price may vary";
 }
-class salad{
+function salad(){
     insalataGiotto = 7.95;
     Insalata_Di_Campo = 7.50;
     Insalata_Caprese = 13.00;
@@ -19,7 +20,7 @@ class salad{
     Insalata_Ceasare_add3 = {FriedCalamari: 6.50};
     Insalata_Mediterranea = 7.95;
 }
-class meat{
+function meat(){
     Pollo_Quattro_Formaggio = 25.50;
     Vitello_Piccata = 29.95;
     Vitello_Alla_Parmiggiana = 28.50;
@@ -29,7 +30,7 @@ class meat{
     Pollo_Alla_Parmiggiana = 25.50;
     SalmonLimone = 26.95;
 }
-class pasta{
+function pasta(){
     Rondelli_Di_Spinaci_E_Ricotta = 19.95;
     GnocchiSorrentina = 19.95;
     Mostaccioli_Bolognese = 22.50;
@@ -42,7 +43,7 @@ class pasta{
     Penne_Alla_Ortolana = 23.50;
     Linguine_Vongole = 25.50;
 }
-class desserts{
+function desserts(){
     Tiramisu = 8.00;
     PannaCotta = 7.75;
     NYCheesecake = {Price: 8.75, Toppings: Chocolate, Raspberry, Blackberry};
