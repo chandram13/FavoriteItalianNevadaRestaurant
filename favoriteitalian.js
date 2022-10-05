7e420719345b48bd672811b5fe415a262d6b99ec
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-05 10:49:59 -0700

    Add files via upload

---
+++ favoriteitalian.js
@@ -0,0 +1,54 @@
+// Marvish Chandra
+
+class appetizers{
+    fritto_misto_italiano = 13.50;
+    calamariFritti = 13.95;
+    olive_Allascolana = 13.95;
+    pizzaDoppia = 15.50;
+    bruschettaFresca = 12.50;
+    Cocktail_Di_Gamberi = 16.60;
+    Salumeria_Da_Matteo = "Create your own, price may vary";
+}
+class salad{
+    insalataGiotto = 7.95;
+    Insalata_Di_Campo = 7.50;
+    Insalata_Caprese = 13.00;
+    Insalata_Ceasare = 8.50;
+    Insalata_Ceasare_add1 = {Anchovies: 2.50};
+    Insalata_Ceasare_add2 = {GrilledChicken: 5.50};
+    Insalata_Ceasare_add3 = {FriedCalamari: 6.50};
+    Insalata_Mediterranea = 7.95;
+}
+class meat{
+    Pollo_Quattro_Formaggio = 25.50;
+    Vitello_Piccata = 29.95;
+    Vitello_Alla_Parmiggiana = 28.50;
+    Filleto_Di_Bue_Gorgonzola = 45.00;
+    Scallops_Valle_Verde = 33.50;
+    Pollo_Amore = 25.50;
+    Pollo_Alla_Parmiggiana = 25.50;
+    SalmonLimone = 26.95;
+}
+class pasta{
+    Rondelli_Di_Spinaci_E_Ricotta = 19.95;
+    GnocchiSorrentina = 19.95;
+    Mostaccioli_Bolognese = 22.50;
+    Spaghetti_Del_Golfo = 26.00;
+    Linguine_Frutti_Di_Mare = 31.75;
+    Tagliatella_Alla_Tarantella = 24.50;
+    Ravioli_Mariarosa = 18.50;
+    Penne_Norcia = 24.50;
+    Tortellini_Matteos = 24.50;
+    Penne_Alla_Ortolana = 23.50;
+    Linguine_Vongole = 25.50;
+}
+class desserts{
+    Tiramisu = 8.00;
+    PannaCotta = 7.75;
+    NYCheesecake = {Price: 8.75, Toppings: Chocolate, Raspberry, Blackberry};
+    DeliziaLimone = 8.00;
+    CremeBrulee = 8.00;
+    FangoCake = 8.75;
+    Cannoli = 8.00;
+    Gelato = {Price: 8.50, Flavors: Mango, WildBerry, Lemon};
+}
